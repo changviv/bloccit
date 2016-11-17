ab26e1f39cdb1823550a0d6c89a974e376c151f3
Vivien Chang <vchang103@gmail.com>
2016-11-17 14:38:14 -0800

    checkpoint 13 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
+   get 'welcome/index'
 
-  # get 'welcome/about'
+   get 'welcome/about'
+
+   root 'welcome#index'
+
+   root 'welcom#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
