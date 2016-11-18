c07a2ea72419e0d80c5fea1622b706f3a8cafd48
Vivien Chang <vchang103@gmail.com>
2016-11-17 16:06:47 -0800

    assignment 13 completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
    get 'welcome/about'
 
+   get 'welcome/faq'
+
    root 'welcome#index'
 
-   root 'welcom#contact'
+   root 'welcom#faq'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
