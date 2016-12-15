9c4f335ed33b819c750ac1883e6f613a6e997adf
Vivien Chang <vchang103@gmail.com>
2016-12-15 15:03:49 -0800

    checkpoint 24 completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
+  resources :users, only: [:new, :create]
+
   get 'about' => 'welcome#about'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
