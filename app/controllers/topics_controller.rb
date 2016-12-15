43c5c086ed88d110115c05bb8dea1b2f8e2c3bd8
Vivien Chang <vchang103@gmail.com>
2016-12-14 17:30:15 -0800

    assignment 23 completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ApplicationController
-  skip_before_filter :verify_authenticity_token  
+  skip_before_filter :verify_authenticity_token
   def index
     @topics = Topic.all
   end
