08d5ddc06605150f92a55776e197d8446e528122
Vivien Chang <vchang103@gmail.com>
2016-12-16 15:19:24 -0800

    assignment 25 completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def new
     @user = User.new
+  end
+
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
   end
 
   def create
     @user = User.new
     @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
+    @user.email = params{[:user][:email]}
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
 
@@ -18,4 +28,4 @@ class UsersController < ApplicationController
        render :new
      end
    end
-  end
+end
