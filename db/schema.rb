d28600f92b1f0ff7a23c435d3f3fd6f5d8c40bea
Vivien Chang <vchang103@gmail.com>
2016-12-19 16:02:40 -0800

    checkpoint 27 completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215044137) do
+ActiveRecord::Schema.define(version: 20161218022450) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -34,7 +34,9 @@ ActiveRecord::Schema.define(version: 20161215044137) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -43,6 +45,16 @@ ActiveRecord::Schema.define(version: 20161215044137) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
   end
 
   create_table "topics", force: :cascade do |t|
