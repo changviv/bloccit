90a2e78f0ecc4beefe18d8b682fee2ac215bf264
Vivien Chang <vchang103@gmail.com>
2016-12-20 19:19:40 -0800

    checkpoint 28 completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,8 @@
 class TopicsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
+  before_action :require_sign_in, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
+
   def index
     @topics = Topic.all
   end
@@ -30,7 +33,6 @@ class TopicsController < ApplicationController
 
   def update
      @topic = Topic.find(params[:id])
-
      @topic = Topic.new(topic_params)
 
      if @topic.save
@@ -58,5 +60,12 @@ class TopicsController < ApplicationController
 
   def topic_params
     params.require(:topic).permit(:name, :description, :public)
+  end
+
+  def authorize_user
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
   end
 end
